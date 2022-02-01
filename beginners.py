9eee6cf568ae68a56ef8c6aa75e397c639b9aa8a
DecisionTree <random.forest83@gmail.com>
2022-02-01 14:46:37 +0100

    Commit icon explanation.

--- beginners.py
+++ beginners.py
@@ -2,6 +2,6 @@ print("Hello GitHub Beginners!")
 
 # In the VS CODE there is built-in function called Source Control to commit and push.
 # Source Control is to be found on the left side - the icon with 3 branches from the top on the icon bar.
-# In the Source Control panel we entered a message and ticked it & Sync Changes
-# commit --> locking the file in
+# In the Source Control panel we entered a message and ticked it (Commit) & Sync Changes
+# commit (the check symbol) --> locking the file in
 # push --> it will take everything on our machine and it will push it online
