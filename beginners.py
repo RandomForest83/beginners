43baef34dbb66bac3788c1d00073c6baa5353848
DecisionTree <random.forest83@gmail.com>
2022-02-01 15:13:59 +0100

    A New Change Made.

--- beginners.py
+++ beginners.py
@@ -5,3 +5,6 @@ print("Hello GitHub Beginners!")
 # In the Source Control panel we entered a message and ticked it (Commit) & Sync Changes
 # commit (the check symbol) --> locking the file in
 # push --> it will take everything on our machine and it will push it online
+# pull --> it downloads the file from the cloud to the personal PC
+# get clone and paste the copied link --> connets to (clones) the folder in GitHub we work on
+
