c0408866c96c63954ee00fe940a0581d0aa16fc6
DecisionTree <random.forest83@gmail.com>
2022-02-01 14:38:29 +0100

    Final version.

--- beginners.py
+++ beginners.py
@@ -1,7 +1,7 @@
 print("Hello GitHub Beginners!")
 
-# In the VS CODE built-in function called Source Control to commit and push.
-# Source Control is to be found on the left side - on the icon bar
-# In the Source Control panel we entered a message and ticked it & SYNC
+# In the VS CODE there is built-in function called Source Control to commit and push.
+# Source Control is to be found on the left side - the icon with 3 branches from the top on the icon bar.
+# In the Source Control panel we entered a message and ticked it & Sync Changes
 # commit --> locking the file in
 # push --> it will take everything on our machine and it will push it online
