f33ad817e355ade4c3b592b0f7d2a142154e78ce
DecisionTree <random.forest83@gmail.com>
2022-02-01 14:34:04 +0100

    Even better now.

--- beginners.py
+++ beginners.py
@@ -1,4 +1,7 @@
 print("Hello GitHub Beginners!")
 
-# git commit → locking the file in
-# git push --> 
+# In the VS CODE built-in function called Source Control to commit and push.
+# Source Control is to be found on the left side - on the icon bar
+# In the Source Control panel we entered a message and ticked it & SYNC
+# commit --> locking the file in
+# push --> it will take everything on our machine and it will push it online
